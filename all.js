edf009e0456503050209385cba8041a813e8df67
Per Liedman <per@liedman.net>
2014-03-25 09:50:42 +0100

    Satellite layer

--- all.js
+++ all.js
@@ -3,7 +3,15 @@ var Backbone = require('backbone'),
     Router = require('./router'),
     Models = require('./models'),
     Views = require('./views'),
-    map = L.mapbox.map('map', 'liedman.hj92n687');
+    map = L.mapbox.map('map');
+
+L.control.layers({
+    'Map': L.mapbox.tileLayer('liedman.hj92n687').addTo(map),
+    'Aerial': L.mapbox.tileLayer('liedman.hj933401')
+},
+{}, {
+    position: 'bottomleft'
+}).addTo(map);
 
 new Router({map: map});
 Backbone.history.start();
