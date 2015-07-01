51c11c05efd4ef97bb72ef64e294d0319b9cd1dd
Per Liedman <per@liedman.net>
2015-07-01 19:55:59 +0200

    Upgrade mapbox.js, switch to tokens etc.

--- all.js
+++ all.js
@@ -2,12 +2,14 @@
 var Backbone = require('backbone'),
     Router = require('./router'),
     Models = require('./models'),
-    Views = require('./views'),
-    map = L.mapbox.map('map');
+    Views = require('./views');
+
+L.mapbox.accessToken = 'pk.eyJ1IjoibGllZG1hbiIsImEiOiJreTdqT1lBIn0.9Diw-C3yp0t8nROiiJT7bA';
+var map = L.mapbox.map('map');
 
 L.control.layers({
-    'Map': L.mapbox.tileLayer('liedman.hj92n687').addTo(map),
-    'Aerial': L.mapbox.tileLayer('liedman.hj933401')
+    'Map': L.mapbox.tileLayer('mapbox.outdoors').addTo(map),
+    'Aerial': L.mapbox.tileLayer('mapbox.satellite')
 },
 {}, {
     position: 'bottomleft'
